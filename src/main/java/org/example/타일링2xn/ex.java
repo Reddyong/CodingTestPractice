35c67115343b2058d44ed5b5471c0242f34d4ce1
Reddyong <d0209hong@naver.com>
2023-10-30 16:13:44 +0900

    feat : 타일링 init

---
+++ src/main/java/org/example/타일링2xn/ex.java
@@ -0,0 +1,15 @@
+package org.example.타일링2xn;
+
+public class ex {
+    public int solution(int n) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ex sol = new ex();
+        int solution = sol.solution(4);
+
+        System.out.println("solution = " + solution);
+    }
+}
