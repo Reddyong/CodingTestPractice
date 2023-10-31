21359b6357c9c2fba57ba86ee452450a0673a3e7
Reddyong <d0209hong@naver.com>
2023-10-31 15:51:58 +0900

    feat : 2xn tiling

--- src/main/java/org/example/타일링2xn/ex.java
+++ src/main/java/org/example/타일링2xn/ex.java
@@ -1,8 +1,20 @@
 package org.example.타일링2xn;
 
+import java.util.Arrays;
+
 public class ex {
     public int solution(int n) {
         int answer = 0;
+        int[] tile = new int[n + 1];
+        tile[0] = 1;
+        tile[1] = 1;
+
+        for (int i = 2; i < tile.length; i++) {
+            tile[i] = tile[i - 1] % 1000000007 + tile[i - 2] % 1000000007;
+        }
+
+        answer = tile[n] % 1000000007;
+
         return answer;
     }
 
