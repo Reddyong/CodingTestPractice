1fa22c58ff90a7975b7ff61772c91cd1fc531679
Reddyong <d0209hong@naver.com>
2023-10-31 16:51:00 +0900

    feat : create 택배상자

---
+++ src/main/java/org/example/택배상자/ex.java
@@ -0,0 +1,17 @@
+package org.example.택배상자;
+
+public class ex {
+    public int solution(int[] order) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ex sol = new ex();
+        int solution1 = sol.solution(new int[]{4, 3, 1, 2, 5});
+        int solution2 = sol.solution(new int[]{5, 4, 3, 2, 1});
+
+        System.out.println("solution1 = " + solution1);
+        System.out.println("solution2 = " + solution2);
+    }
+}
