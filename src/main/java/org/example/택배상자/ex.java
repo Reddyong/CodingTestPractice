515641626ae3ab850ac7a6b51b3dfb29ce73defc
Reddyong <d0209hong@naver.com>
2023-11-01 13:13:42 +0900

    feat : changeToReal()

--- src/main/java/org/example/택배상자/ex.java
+++ src/main/java/org/example/택배상자/ex.java
@@ -1,17 +1,33 @@
 package org.example.택배상자;
 
+import java.util.Stack;
+
 public class ex {
+    int[] realBelt;
     public int solution(int[] order) {
         int answer = 0;
+        realBelt = new int[order.length];
+        Stack<Integer> subBelt = new Stack<>();
+
+        changeToReal(order);
+
+
         return answer;
+    }
+
+    public void changeToReal(int[] order) {
+        for (int i = 0; i < order.length; i++) {
+            int index = order[i] - 1;
+            realBelt[index] = i + 1;
+        }
     }
 
     public static void main(String[] args) {
         ex sol = new ex();
         int solution1 = sol.solution(new int[]{4, 3, 1, 2, 5});
-        int solution2 = sol.solution(new int[]{5, 4, 3, 2, 1});
+//        int solution2 = sol.solution(new int[]{5, 4, 3, 2, 1});
 
         System.out.println("solution1 = " + solution1);
-        System.out.println("solution2 = " + solution2);
+//        System.out.println("solution2 = " + solution2);
     }
 }
