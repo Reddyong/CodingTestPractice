502bdac546f08a42aa7122ecbdff974732d2b59f
Reddyong <d0209hong@naver.com>
2023-11-01 14:34:24 +0900

    fix : delete all method

--- src/main/java/org/example/택배상자/ex.java
+++ src/main/java/org/example/택배상자/ex.java
@@ -3,86 +3,22 @@ package org.example.택배상자;
 import java.util.*;
 
 public class ex {
-    int[] realBelt;
-    List<Integer> truck = new ArrayList<>();
-    Queue<Integer> belt = new LinkedList<>();
-    Stack<Integer> subBelt = new Stack<>();
     public int solution(int[] order) {
         int answer = 0;
-        realBelt = new int[order.length];
 
-        changeToReal(order);
-
-        putInSub();
-
-        putFirst();
-
-        putInTruck();
-
-        answer = truck.size();
 
         return answer;
     }
 
-    public void putFirst() {
-        truck.add(belt.poll());
-    }
-
-    public void changeToReal(int[] order) {
-        for (int i = 0; i < order.length; i++) {
-            int index = order[i] - 1;
-            realBelt[index] = i + 1;
-        }
-
-        for (int j = 0; j < realBelt.length; j++) {
-            belt.add(realBelt[j]);
-        }
-
-    }
-
-    public void putInSub() {
-        for (int i = 0; i < realBelt.length ; i++) {
-            if (belt.peek() == 1) {
-                break;
-            }
-            Integer poll = belt.poll();
-            subBelt.push(poll);
-        }
-    }
-
-    public void putInTruck() {
-        for (int i = 2; i < realBelt.length + 1; i++) {
-            checkBelt(i);
-            checkSubBelt(i);
-        }
-    }
-
-    public void checkBelt(int i) {
-        if (belt.isEmpty()) {
-            return;
-        }
-        if (belt.peek() == i && truck.contains(i - 1)) {
-            belt.poll();
-            truck.add(i);
-        }
-    }
-
-    public void checkSubBelt(int i) {
-        if (subBelt.isEmpty()) {
-            return;
-        }
-        if (subBelt.peek() == i && truck.contains(i - 1)) {
-            subBelt.pop();
-            truck.add(i);
-        }
-    }
 
     public static void main(String[] args) {
         ex sol = new ex();
-        int solution1 = sol.solution(new int[]{4, 3, 1, 2, 5});
+//        int solution1 = sol.solution(new int[]{4, 3, 1, 2, 5});
 //        int solution2 = sol.solution(new int[]{5, 4, 3, 2, 1});
+        int solution3 = sol.solution(new int[]{3, 5, 4, 2, 1});
 
-        System.out.println("solution1 = " + solution1);
+//        System.out.println("solution1 = " + solution1);
 //        System.out.println("solution2 = " + solution2);
+        System.out.println("solution3 = " + solution3);
     }
 }
