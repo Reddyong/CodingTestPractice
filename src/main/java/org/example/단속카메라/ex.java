4a71a494deb4fa91e905db4f465b8cd057be8413
Reddyong <d0209hong@naver.com>
2023-11-07 15:21:38 +0900

    feat : create 단속카메라

---
+++ src/main/java/org/example/단속카메라/ex.java
@@ -0,0 +1,15 @@
+package org.example.단속카메라;
+
+public class ex {
+    public int solution(int[][] routes) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ex sol = new ex();
+        int solution = sol.solution(new int[][]{{-20, -15}, {-14, -5}, {-18, -13}, {-5, -3}});
+
+        System.out.println("solution = " + solution);
+    }
+}
