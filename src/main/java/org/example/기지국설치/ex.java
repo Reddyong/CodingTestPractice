b7195650a4cdbfc11f72eba19354fb6ea77e5073
Reddyong <d0209hong@naver.com>
2023-11-20 15:52:07 +0900

    feat : answer

--- src/main/java/org/example/기지국설치/ex.java
+++ src/main/java/org/example/기지국설치/ex.java
@@ -3,6 +3,18 @@ package org.example.기지국설치;
 public class ex {
     public int solution(int n, int[] stations, int w) {
         int answer = 0;
+        int now = 1;
+        int stationIndex = 0;
+
+        while (now <= n) {
+            if (stationIndex >= stations.length || now < stations[stationIndex] - w) {
+                answer++;
+                now = now + 2 * w + 1;
+            } else {
+                now = stations[stationIndex] + w + 1;
+                stationIndex++;
+            }
+        }
 
         return answer;
     }
