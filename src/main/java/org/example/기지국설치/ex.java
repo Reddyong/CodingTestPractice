400523b67ec11d55275a004657350135ef985773
Reddyong <d0209hong@naver.com>
2023-11-20 13:32:49 +0900

    feat : create 기지국설치

---
+++ src/main/java/org/example/기지국설치/ex.java
@@ -0,0 +1,20 @@
+package org.example.기지국설치;
+
+public class ex {
+    public int solution(int n, int[] stations, int w) {
+        int answer = 0;
+
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ex sol = new ex();
+        int solution1 = sol.solution(11, new int[]{4, 11}, 1);
+        int solution2 = sol.solution(16, new int[]{9}, 2);
+
+        System.out.println("solution1 = " + solution1);
+        System.out.println("solution2 = " + solution2);
+    }
+}
