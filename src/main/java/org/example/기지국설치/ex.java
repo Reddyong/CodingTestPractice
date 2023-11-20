7eea0fd52a0341271d890b6ca2e966ace6b6df20
Reddyong <d0209hong@naver.com>
2023-11-20 15:02:15 +0900

    fix : delete all

--- src/main/java/org/example/기지국설치/ex.java
+++ src/main/java/org/example/기지국설치/ex.java
@@ -3,35 +3,8 @@ package org.example.기지국설치;
 public class ex {
     public int solution(int n, int[] stations, int w) {
         int answer = 0;
-        boolean[] check = new boolean[n];
-
-        init(stations, check);
-
-        isStationsTrue(n, stations, w, check);
 
         return answer;
-    }
-
-    private static void isStationsTrue(int n, int[] stations, int w, boolean[] check) {
-        for (int station : stations) {
-            int location = station - 1;
-            if (location - w > 0) {
-                for (int i = location - w; i < location; i++) {
-                    check[i] = true;
-                }
-            }
-            if (location + w < n) {
-                for (int j = location + 1; j < location + 1 + w; j++) {
-                    check[j] = true;
-                }
-            }
-        }
-    }
-
-    private static void init(int[] stations, boolean[] check) {
-        for (int station : stations) {
-            check[station - 1] = true;
-        }
     }
 
     public static void main(String[] args) {
