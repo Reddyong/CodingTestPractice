2c1c8f78224eb0cdfac999c55c4a0926cc404588
Reddyong <d0209hong@naver.com>
2023-11-22 12:48:12 +0900

    feat : isLastLine()

--- src/main/java/org/example/삼각달팽이/ex.java
+++ src/main/java/org/example/삼각달팽이/ex.java
@@ -1,9 +1,25 @@
 package org.example.삼각달팽이;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ex {
     public int[] solution(int n) {
         int[] answer = init(n);
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            isLastLine(n, list, i);
+        }
         return answer;
+    }
+
+    private static void isLastLine(int n, List<Integer> list, int i) {
+        if (i == n) {
+            for (int j = 0; j < n; j++) {
+                list.add(i + j);
+            }
+        }
     }
 
     private static int[] init(int n) {
