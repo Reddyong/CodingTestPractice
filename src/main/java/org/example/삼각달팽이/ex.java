87e1441a0f86446c2ab873d779db2d9319be0e2e
Reddyong <d0209hong@naver.com>
2023-11-22 12:31:51 +0900

    feat : create 삼각달팽이

---
+++ src/main/java/org/example/삼각달팽이/ex.java
@@ -0,0 +1,15 @@
+package org.example.삼각달팽이;
+
+public class ex {
+    public int[] solution(int n) {
+        int[] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ex sol = new ex();
+        int[] solution1 = sol.solution(4);
+        int[] solution2 = sol.solution(5);
+        int[] solution3 = sol.solution(6);
+    }
+}
