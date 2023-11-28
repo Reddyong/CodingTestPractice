5edecade968dbe109c902357b4e7a27c41d2bdbc
Reddyong <d0209hong@naver.com>
2023-11-28 13:53:28 +0900

    fix : 반복문 탈출 조건 수정

--- src/main/java/org/example/두큐합같게만들기/ex.java
+++ src/main/java/org/example/두큐합같게만들기/ex.java
@@ -10,16 +10,18 @@ public class ex {
     Queue<Integer> q2 = new LinkedList<>();
     public int solution(int[] queue1, int[] queue2) {
         int answer = 0;
+        int count = 0;
 
         init(queue1, queue2);
 
         while (sum1 != sum2) {
-            answer = whereIsBigSum(answer);
-
-            if (q1.isEmpty() || q2.isEmpty()) {
+            if (count >= (queue1.length + queue2.length) * 2) {
                 answer = -1;
                 break;
             }
+
+            answer = whereIsBigSum(answer);
+            count++;
         }
 
         return answer;
@@ -55,11 +57,11 @@ public class ex {
     public static void main(String[] args) {
         ex sol = new ex();
 //        int solution1 = sol.solution(new int[]{3, 2, 7, 2}, new int[]{4, 6, 5, 1});
-//        int solution2 = sol.solution(new int[]{1, 2, 1, 2}, new int[]{1, 10, 1, 2});
-        int solution3 = sol.solution(new int[]{1, 1}, new int[]{1, 5});
+        int solution2 = sol.solution(new int[]{1, 2, 1, 2}, new int[]{1, 10, 1, 2});
+//        int solution3 = sol.solution(new int[]{1, 1}, new int[]{1, 5});
 
 //        System.out.println("solution1 = " + solution1);
-//        System.out.println("solution2 = " + solution2);
-        System.out.println("solution3 = " + solution3);
+        System.out.println("solution2 = " + solution2);
+//        System.out.println("solution3 = " + solution3);
     }
 }
