d33df11ba35da5cf1bcf4da0476b81b806817b8e
Reddyong <d0209hong@naver.com>
2023-11-28 13:15:48 +0900

    feat : init()

--- src/main/java/org/example/두큐합같게만들기/ex.java
+++ src/main/java/org/example/두큐합같게만들기/ex.java
@@ -1,15 +1,40 @@
 package org.example.두큐합같게만들기;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class ex {
+    long sum1 = 0;
+    long sum2 = 0;
+    Queue<Integer> q1 = new LinkedList<>();
+    Queue<Integer> q2 = new LinkedList<>();
     public int solution(int[] queue1, int[] queue2) {
         int answer = -2;
+
+        init(queue1, queue2);
+
         return answer;
+    }
+
+    public void init(int[] queue1, int[] queue2) {
+        for (int i : queue1) {
+            q1.add(i);
+            sum1 += i;
+        }
+        for (int i : queue2) {
+            q2.add(i);
+            sum2 += i;
+        }
     }
 
     public static void main(String[] args) {
         ex sol = new ex();
         int solution1 = sol.solution(new int[]{3, 2, 7, 2}, new int[]{4, 6, 5, 1});
-        int solution2 = sol.solution(new int[]{1, 2, 1, 2}, new int[]{1, 10, 1, 2});
-        int solution3 = sol.solution(new int[]{1, 1}, new int[]{1, 5});
+//        int solution2 = sol.solution(new int[]{1, 2, 1, 2}, new int[]{1, 10, 1, 2});
+//        int solution3 = sol.solution(new int[]{1, 1}, new int[]{1, 5});
+
+        System.out.println("solution1 = " + solution1);
+//        System.out.println("solution2 = " + solution2);
+//        System.out.println("solution3 = " + solution3);
     }
 }
