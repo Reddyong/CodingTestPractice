38bb8ae5fc1b826e599d18af66afb6d2eee87b91
Reddyong <d0209hong@naver.com>
2023-12-08 16:07:16 +0900

    feat : final answer

--- src/main/java/org/example/나라124의숫자/ex.java
+++ src/main/java/org/example/나라124의숫자/ex.java
@@ -1,8 +1,33 @@
 package org.example.나라124의숫자;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ex {
     public String solution(int n) {
         String answer = "";
+        List<Integer> list = new ArrayList<>();
+
+        while (n != 0) {
+            int num = n % 3;
+            if (num == 0) {
+                num = 3;
+                list.add(num);
+                n = n / 3 - 1;
+                continue;
+            }
+            list.add(num);
+            n /= 3;
+        }
+
+        for (int i = list.size() - 1; i >= 0; i--) {
+            Integer integer = list.get(i);
+            if (integer.equals(3)) {
+                integer = 4;
+            }
+            answer += integer;
+        }
+
         return answer;
     }
 
@@ -11,7 +36,7 @@ public class ex {
         String solution1 = sol.solution(1);
         String solution2 = sol.solution(2);
         String solution3 = sol.solution(3);
-        String solution4 = sol.solution(4);
+        String solution4 = sol.solution(6);
 
         System.out.println("solution1 = " + solution1);
         System.out.println("solution2 = " + solution2);
