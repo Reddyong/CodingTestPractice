6c61f421a82c96288d209700dee7e46f8c2a19d6
Reddyong <d0209hong@naver.com>
2023-12-08 15:20:51 +0900

    feat : create 124나라의 숫자

---
+++ src/main/java/org/example/나라124의숫자/ex.java
@@ -0,0 +1,21 @@
+package org.example.나라124의숫자;
+
+public class ex {
+    public String solution(int n) {
+        String answer = "";
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ex sol = new ex();
+        String solution1 = sol.solution(1);
+        String solution2 = sol.solution(2);
+        String solution3 = sol.solution(3);
+        String solution4 = sol.solution(4);
+
+        System.out.println("solution1 = " + solution1);
+        System.out.println("solution2 = " + solution2);
+        System.out.println("solution3 = " + solution3);
+        System.out.println("solution4 = " + solution4);
+    }
+}
