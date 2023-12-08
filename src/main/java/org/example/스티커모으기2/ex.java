2143060b49bf64001d3e6133fa0a25586f43c872
Reddyong <d0209hong@naver.com>
2023-12-08 16:36:32 +0900

    feat : create 스티커모으기2

---
+++ src/main/java/org/example/스티커모으기2/ex.java
@@ -0,0 +1,15 @@
+package org.example.스티커모으기2;
+
+public class ex {
+    public int solution(int sticker[]) {
+        int answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ex sol = new ex();
+        int solution1 = sol.solution(new int[]{14, 6, 5, 11, 3, 9, 2, 10});
+        int solution2 = sol.solution(new int[]{1, 3, 2, 5, 4});
+    }
+}
