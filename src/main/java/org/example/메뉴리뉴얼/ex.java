569160ff2c97e987536fa128aa6d96916cdb0c18
Reddyong <d0209hong@naver.com>
2023-12-28 16:27:41 +0900

    feat : checkOrders() -> 주문한 메뉴들이 무엇이 있는지 menu List에 추가

--- src/main/java/org/example/메뉴리뉴얼/ex.java
+++ src/main/java/org/example/메뉴리뉴얼/ex.java
@@ -1,11 +1,37 @@
 package org.example.메뉴리뉴얼;
 
-import java.util.Arrays;
+import java.util.*;
+import java.util.stream.Stream;
 
 public class ex {
     public String[] solution(String[] orders, int[] course) {
         String[] answer = {};
+        List<String> menu = new ArrayList<>();
+        List<String> answerList = new ArrayList<>();
+
+        checkOrders(orders, menu);
+
+        System.out.println("menu = " + menu);
+
         return answer;
+    }
+
+    private static void checkOrders(String[] orders, List<String> menu) {
+        for (String order : orders) {
+            addMenu(menu, order);
+        }
+        menu.sort(Comparator.naturalOrder());
+    }
+
+    private static void addMenu(List<String> menu, String order) {
+        for (int i = 0; i < order.length(); i++) {
+            String[] split = order.split("");
+            for (String s : split) {
+                if (!menu.contains(s)) {
+                    menu.add(s);
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
