af6f9d4c5fdb1be4820b222aa067d7a603835970
Reddyong <d0209hong@naver.com>
2024-01-02 14:12:14 +0900

    feat : final answer

--- src/main/java/org/example/메뉴리뉴얼/ex.java
+++ src/main/java/org/example/메뉴리뉴얼/ex.java
@@ -4,35 +4,48 @@ import java.util.*;
 import java.util.stream.Stream;
 
 public class ex {
+    Map<String, Integer> map;
+    int max;
     public String[] solution(String[] orders, int[] course) {
         String[] answer = {};
-        List<String> menu = new ArrayList<>();
         List<String> answerList = new ArrayList<>();
 
-        checkOrders(orders, menu);
+        for (int c : course) {
+            map = new HashMap<>();
+            max = 0;
 
-        System.out.println("menu = " + menu);
+            for (String order : orders) {
+                char[] charArray = order.toCharArray();
+                Arrays.sort(charArray);
+                order = new String(charArray);
+                solve(0, -1, order, "", c);
+            }
+
+            for (String key : map.keySet()) {
+                Integer val = map.get(key);
+                if (val > 1 && val == max) {
+                    answerList.add(key);
+                }
+            }
+        }
+
+        Collections.sort(answerList);
+        answer = answerList.toArray(new String[answerList.size()]);
 
         return answer;
     }
 
-    private static void checkOrders(String[] orders, List<String> menu) {
-        for (String order : orders) {
-            addMenu(menu, order);
+    private void solve(int curStep, int preIdx, String order, String cur, int cnt) {
+        if (curStep == cnt) {
+            map.put(cur, map.getOrDefault(cur, 0) + 1);
+            max = Math.max(max, map.get(cur));
         }
-        menu.sort(Comparator.naturalOrder());
+
+        for (int i = preIdx + 1; i < order.length(); i++) {
+            solve(curStep + 1, i, order, cur + order.charAt(i), cnt);
+        }
     }
 
-    private static void addMenu(List<String> menu, String order) {
-        for (int i = 0; i < order.length(); i++) {
-            String[] split = order.split("");
-            for (String s : split) {
-                if (!menu.contains(s)) {
-                    menu.add(s);
-                }
-            }
-        }
-    }
 
     public static void main(String[] args) {
         ex sol = new ex();
