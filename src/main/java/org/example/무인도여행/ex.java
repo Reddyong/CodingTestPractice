251d9a23395bde90dd75c5a4bc42c68d2f7af6f4
Reddyong <d0209hong@naver.com>
2024-01-03 12:53:09 +0900

    feat : create 무인도여행

---
+++ src/main/java/org/example/무인도여행/ex.java
@@ -0,0 +1,19 @@
+package org.example.무인도여행;
+
+import java.util.Arrays;
+
+public class ex {
+    public int[] solution(String[] maps) {
+        int[] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ex sol = new ex();
+        int[] solution1 = sol.solution(new String[]{"X591X", "X1X5X", "X231X", "1XXX1"});
+        int[] solution2 = sol.solution(new String[]{"XXX", "XXX", "XXX"});
+
+        System.out.println("solution1 = " + Arrays.toString(solution1));
+        System.out.println("solution2 = " + Arrays.toString(solution2));
+    }
+}
