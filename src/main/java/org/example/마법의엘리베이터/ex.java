8bd466d01667a10d9ed79d647424d2c67a3597e4
Reddyong <d0209hong@naver.com>
2024-01-15 14:52:21 +0900

    feat : initial commit of 마법의 엘리베이터

---
+++ src/main/java/org/example/마법의엘리베이터/ex.java
@@ -0,0 +1,18 @@
+package org.example.마법의엘리베이터;
+
+public class ex {
+    public int solution(int storey){
+        int answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ex sol = new ex();
+        int solution1 = sol.solution(16);
+        int solution2 = sol.solution(2554);
+
+        System.out.println("solution1 = " + solution1);
+        System.out.println("solution2 = " + solution2);
+    }
+}
