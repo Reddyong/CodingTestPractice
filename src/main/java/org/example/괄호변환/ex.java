c6b91209802ea9aacdef7a0b1b22c7354a394ce0
Reddyong <d0209hong@naver.com>
2024-01-16 15:07:43 +0900

    feat : checkCorrect() -> checking correct String u : if is correct returns true, else returns false

--- src/main/java/org/example/괄호변환/ex.java
+++ src/main/java/org/example/괄호변환/ex.java
@@ -3,8 +3,38 @@ package org.example.괄호변환;
 public class ex {
     public String solution(String p) {
         String answer = "";
+
+        if (checkCorrect(p)) {
+            return p;
+        }
+
         return answer;
     }
+
+    private boolean checkCorrect(String u) {
+        if (u.substring(0, 1).equals(")")) {
+            return false;
+        }
+
+        int len = u.length();
+        int left = 0;
+        int right = 0;
+
+        for (int i = 0; i < len; i++) {
+            if (u.substring(i, i + 1).equals("(")) {
+                left++;
+            } else {
+                right++;
+            }
+
+            if (right > left) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
 
     public static void main(String[] args) {
         ex sol = new ex();
