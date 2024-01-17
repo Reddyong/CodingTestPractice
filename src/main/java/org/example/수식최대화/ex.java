81ef4b6c3e0e07df2a60576c0d4f91b97386e790
Reddyong <d0209hong@naver.com>
2024-01-17 13:35:32 +0900

    feat : initial commit of 수식 최대화

---
+++ src/main/java/org/example/수식최대화/ex.java
@@ -0,0 +1,17 @@
+package org.example.수식최대화;
+
+public class ex {
+    public long solution(String expression) {
+        long answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ex sol = new ex();
+        long solution1 = sol.solution("100-200*300-500+20");
+        long solution2 = sol.solution("50*6-3*2");
+
+        System.out.println("solution1 = " + solution1);
+        System.out.println("solution2 = " + solution2);
+    }
+}
