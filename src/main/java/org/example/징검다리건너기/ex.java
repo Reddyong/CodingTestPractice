4746062c561ff938926c06038458ac93acdd07c3
Reddyong <d0209hong@naver.com>
2024-01-18 12:37:03 +0900

    feat : initial commit 징검다리 건너기

---
+++ src/main/java/org/example/징검다리건너기/ex.java
@@ -0,0 +1,15 @@
+package org.example.징검다리건너기;
+
+public class ex {
+    public int solution(int[] stones, int k) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ex sol = new ex();
+        int solution = sol.solution(new int[]{2, 4, 5, 3, 2, 1, 4, 2, 5, 1}, 3);
+
+        System.out.println("solution = " + solution);
+    }
+}
