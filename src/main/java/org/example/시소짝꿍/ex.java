1d2734da86c04b078986745e6246ed58bcc1325b
Reddyong <d0209hong@naver.com>
2024-01-22 14:18:02 +0900

    feat : initial commit of 시소 짝꿍

---
+++ src/main/java/org/example/시소짝꿍/ex.java
@@ -0,0 +1,15 @@
+package org.example.시소짝꿍;
+
+public class ex {
+    public long solution(int[] weights) {
+        long answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ex sol = new ex();
+        long solution = sol.solution(new int[]{100, 180, 360, 100, 270});
+
+        System.out.println("solution = " + solution);
+    }
+}
