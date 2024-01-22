d281581e924044949a72cd1ec0bcfb6ebd85cdb3
Reddyong <d0209hong@naver.com>
2024-01-22 13:43:15 +0900

    feat : final answer of 징검다리 건너기

--- src/main/java/org/example/징검다리건너기/ex.java
+++ src/main/java/org/example/징검다리건너기/ex.java
@@ -1,9 +1,46 @@
 package org.example.징검다리건너기;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ex {
     public int solution(int[] stones, int k) {
         int answer = 0;
+        int result = 0;
+        int low = 0;
+        int high = Integer.MAX_VALUE;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (isCorrect(stones, mid, k)) {
+                low = mid + 1;
+                result = mid;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        answer = result;
+
         return answer;
+    }
+
+    private boolean isCorrect(int[] stones, int mid, int k) {
+        int count = 0;
+
+        for (int i = 0; i < stones.length; i++) {
+            if (stones[i] < mid) {
+                count++;
+                if (count >= k) {
+                    return false;
+                }
+            } else {
+                count = 0;
+            }
+        }
+
+        return true;
     }
 
     public static void main(String[] args) {
