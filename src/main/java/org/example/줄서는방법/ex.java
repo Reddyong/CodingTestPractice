d7b0c2888454f4d61ece119b4bd333fd56dd7a2d
Reddyong <d0209hong@naver.com>
2024-01-25 15:07:07 +0900

    feat : initial commit of 줄서는 방법

---
+++ src/main/java/org/example/줄서는방법/ex.java
@@ -0,0 +1,17 @@
+package org.example.줄서는방법;
+
+import java.util.Arrays;
+
+public class ex {
+    public int[] solution(int n, long k) {
+        int[] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ex sol = new ex();
+        int[] solution = sol.solution(3, 5);
+
+        System.out.println("solution = " + Arrays.toString(solution));
+    }
+}
