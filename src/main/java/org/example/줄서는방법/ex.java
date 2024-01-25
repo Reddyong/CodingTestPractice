db7c58ee022c6a17c7ca4e397944b2d035d5aba8
Reddyong <d0209hong@naver.com>
2024-01-25 16:38:57 +0900

    feat : final answer of 줄서는 방법

--- src/main/java/org/example/줄서는방법/ex.java
+++ src/main/java/org/example/줄서는방법/ex.java
@@ -1,11 +1,37 @@
 package org.example.줄서는방법;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class ex {
+    int count = 0;
     public int[] solution(int n, long k) {
-        int[] answer = {};
+        int[] answer = new int[n];
+        List<Integer> list = new ArrayList<>();
+        long cases = factorial(n);
+
+        for (int i = 1; i <= n; i++) {
+            list.add(i);
+        }
+
+        k--;
+
+        while (count < n) {
+            cases /= (n - count);
+            answer[count++] = list.remove((int) (k / cases));
+            k %= cases;
+        }
+
         return answer;
+    }
+
+    private long factorial(int n) {
+        if (n == 1) {
+            return 1;
+        }
+
+        return n * factorial(n - 1);
     }
 
     public static void main(String[] args) {
