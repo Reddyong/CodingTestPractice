2009cc97e1d0bd43f1f1557235ef6d2725a08eda
Reddyong <d0209hong@naver.com>
2024-01-29 12:55:37 +0900

    feat : initial commit of 숫자카드 나누기

---
+++ src/main/java/org/example/숫자카드나누기/ex.java
@@ -0,0 +1,19 @@
+package org.example.숫자카드나누기;
+
+public class ex {
+    public int solution(int[] arrayA, int[] arrayB) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ex sol = new ex();
+        int solution1 = sol.solution(new int[]{10, 17}, new int[]{5, 20});
+        int solution2 = sol.solution(new int[]{10, 20}, new int[]{5, 17});
+        int solution3 = sol.solution(new int[]{14, 35, 119}, new int[]{18, 30, 102});
+
+        System.out.println("solution1 = " + solution1);
+        System.out.println("solution2 = " + solution2);
+        System.out.println("solution3 = " + solution3);
+    }
+}
