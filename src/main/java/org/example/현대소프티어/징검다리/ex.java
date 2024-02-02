4c24a81efc8c8d4caced6e58087bd4781559adb4
Reddyong <d0209hong@naver.com>
2024-02-02 21:15:32 +0900

    fix : init() -> add dp[] init

--- src/main/java/org/example/현대소프티어/징검다리/ex.java
+++ src/main/java/org/example/현대소프티어/징검다리/ex.java
@@ -10,9 +10,16 @@ public class ex {
     static int N;
     static int result;
     static int[] stones;
+    static int[] dp;
+
     public static void main(String[] args) throws IOException {
 
         init();
+        getAnswer();
+    }
+
+    private static void getAnswer() {
+
     }
 
     private static void init() throws IOException {
@@ -22,6 +29,8 @@ public class ex {
         N = Integer.parseInt(st.nextToken());
         result = 0;
         stones = new int[N];
+        dp = new int[N];
+        Arrays.fill(dp, 1);
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
