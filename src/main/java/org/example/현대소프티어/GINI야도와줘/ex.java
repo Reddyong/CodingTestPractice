bf31db7b21726ab0872501a56c7d54dfa92fb5fa
Reddyong <d0209hong@naver.com>
2024-02-02 13:37:57 +0900

    feat : init() -> save the R and C, map of problem by using Scanner

--- src/main/java/org/example/현대소프티어/GINI야도와줘/ex.java
+++ src/main/java/org/example/현대소프티어/GINI야도와줘/ex.java
@@ -1,8 +1,40 @@
 package org.example.현대소프티어.GINI야도와줘;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ex {
+    static int result = -1;
+    static int temp = -1;
+    static int R;
+    static int C;
+    static String[][] map;
+    static int[] wash = new int[2];
 
     public static void main(String[] args) {
 
+        init();
+
+    }
+
+    private static void init() {
+        Scanner scanner = new Scanner(System.in);
+        R = scanner.nextInt();
+        C = scanner.nextInt();
+        map = new String[R][C];
+
+        for (int i = 0; i < map.length; i++) {
+            String cur = scanner.next();
+            String[] split = cur.split("");
+            for (int j = 0; j < map[i].length; j++) {
+                if (split[j].equals("W")) {
+                    wash[0] = i;
+                    wash[1] = j;
+                }
+                map[i][j] = split[j];
+            }
+        }
+
+        scanner.close();
     }
 }
