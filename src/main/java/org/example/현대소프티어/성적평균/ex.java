e9502685fbb0489ae33ed4b08384bfbe32118c93
Reddyong <d0209hong@naver.com>
2024-02-02 15:33:03 +0900

    feat : init() -> input N and K, and scores, ranges by using scanner -> initiate scores, ranges and results Array

--- src/main/java/org/example/현대소프티어/성적평균/ex.java
+++ src/main/java/org/example/현대소프티어/성적평균/ex.java
@@ -1,7 +1,35 @@
 package org.example.현대소프티어.성적평균;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ex {
+    static int N;
+    static int K;
+    static int[] scores;
+    static int[][] ranges;
+    static double[] results;
     public static void main(String[] args) {
 
+        init();
+    }
+
+    private static void init() {
+        Scanner scanner = new Scanner(System.in);
+
+        N = scanner.nextInt();
+        K = scanner.nextInt();
+
+        scores = new int[N];
+        ranges = new int[K][2];
+        results = new double[K];
+
+        for (int i = 0; i < N; i++) {
+            scores[i] = scanner.nextInt();
+        }
+        for (int j = 0; j < K; j++) {
+            ranges[j][0] = scanner.nextInt();
+            ranges[j][1] = scanner.nextInt();
+        }
     }
 }
