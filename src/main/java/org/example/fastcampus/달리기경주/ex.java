cf97984032cff370fac1372f690a5da42341b5e3
Reddyong <d0209hong@naver.com>
2024-02-07 15:45:39 +0900

    feat : init() -> init map which contains playerName and rank

--- src/main/java/org/example/fastcampus/달리기경주/ex.java
+++ src/main/java/org/example/fastcampus/달리기경주/ex.java
@@ -1,10 +1,21 @@
 package org.example.fastcampus.달리기경주;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class ex {
+    Map<String, Integer> map = new HashMap<>();
     public String[] solution(String[] players, String[] callings) {
+        String[] answer = {};
 
+        init(players);
+
+        return answer;
+    }
+
+    private void init(String[] players) {
+        for (int i = 0; i < players.length; i++) {
+            map.put(players[i], i);
+        }
     }
 
     public static void main(String[] args) {
