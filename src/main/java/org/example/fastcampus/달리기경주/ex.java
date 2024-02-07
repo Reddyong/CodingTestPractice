360ddbd2583d37acdb6d8012c0c108bbbe77099c
Reddyong <d0209hong@naver.com>
2024-02-07 15:00:56 +0900

    feat : initial commit of 달리기 경주

---
+++ src/main/java/org/example/fastcampus/달리기경주/ex.java
@@ -0,0 +1,17 @@
+package org.example.fastcampus.달리기경주;
+
+import java.util.Arrays;
+
+public class ex {
+    public String[] solution(String[] players, String[] callings) {
+
+    }
+
+    public static void main(String[] args) {
+        ex sol = new ex();
+        String[] solution = sol.solution(new String[]{"mumu", "soe", "poe", "kai", "mine"}, new String[]{"kai", "kai", "mine", "mine"});
+
+        System.out.println("solution = " + Arrays.toString(solution));
+
+    }
+}
