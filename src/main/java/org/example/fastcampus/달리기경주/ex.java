e58da7b62fcb1945d34e915eb4f99f79a7cfe507
Reddyong <d0209hong@naver.com>
2024-02-07 15:48:43 +0900

    feat : getAnswer() -> get final answer -> changing rank at map and change player at players[]

--- src/main/java/org/example/fastcampus/달리기경주/ex.java
+++ src/main/java/org/example/fastcampus/달리기경주/ex.java
@@ -5,11 +5,27 @@ import java.util.*;
 public class ex {
     Map<String, Integer> map = new HashMap<>();
     public String[] solution(String[] players, String[] callings) {
-        String[] answer = {};
+        String[] answer = new String[players.length];
 
         init(players);
+        answer = getAnswer(players, callings);
 
         return answer;
+    }
+
+    private String[] getAnswer(String[] players, String[] callings) {
+        for (String c : callings) {
+            Integer rank = map.get(c);
+            String front = players[rank - 1];
+
+            map.put(front, rank);
+            map.put(c, rank - 1);
+
+            players[rank] = front;
+            players[rank - 1] = c;
+        }
+
+        return players;
     }
 
     private void init(String[] players) {
