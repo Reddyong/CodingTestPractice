02d36bdb7bdd75f280a8330bd4a190cbd26737a0
Reddyong <d0209hong@naver.com>
2024-02-13 16:04:18 +0900

    feat : initial commit of 덧칠하기

---
+++ src/main/java/org/example/fastcampus/덧칠하기/ex.java
@@ -0,0 +1,19 @@
+package org.example.fastcampus.덧칠하기;
+
+public class ex {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ex sol = new ex();
+        int solution1 = sol.solution(8, 4, new int[]{2, 3, 6});
+        int solution2 = sol.solution(5, 4, new int[]{1, 3});
+        int solution3 = sol.solution(4, 1, new int[]{1, 2, 3, 4});
+
+        System.out.println("solution1 = " + solution1);
+        System.out.println("solution2 = " + solution2);
+        System.out.println("solution3 = " + solution3);
+    }
+}
