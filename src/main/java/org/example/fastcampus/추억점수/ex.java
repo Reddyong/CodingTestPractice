4528a64084a8dbad7739bdfb36ec1db129c78f0d
Reddyong <d0209hong@naver.com>
2024-02-15 00:09:15 +0900

    feat : init() -> initialize map by using name[] and yearning[] -> key : name, value : yearning

--- src/main/java/org/example/fastcampus/추억점수/ex.java
+++ src/main/java/org/example/fastcampus/추억점수/ex.java
@@ -2,17 +2,28 @@ package org.example.fastcampus.추억점수;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ex {
+    Map<String, Integer> map = new HashMap<>();
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
-        int[] answer = {};
+        int[] answer = new int[photo.length];
+
+        init(name, yearning);
 
         return answer;
     }
 
+    private void init(String[] name, int[] yearning) {
+        for (int i = 0; i < name.length; i++) {
+            map.put(name[i], yearning[i]);
+        }
+    }
+
     public static void main(String[] args) {
         ex sol = new ex();
-        int[] solution1 = sol.solution(new String[]{"may", "kein", "kain"}, new int[]{5, 10, 1, 3}, new String[][]{{"may", "kein", "kain", "radi"}, {"may", "kein", "brin", "deny"}, {"kon", "kain", "may", "coni"}});
+        int[] solution1 = sol.solution(new String[]{"may", "kein", "kain", "radi"}, new int[]{5, 10, 1, 3}, new String[][]{{"may", "kein", "kain", "radi"}, {"may", "kein", "brin", "deny"}, {"kon", "kain", "may", "coni"}});
         int[] solution2 = sol.solution(new String[]{"kali", "mari", "don"}, new int[]{11, 1, 55}, new String[][]{{"kali", "mari", "don"}, {"pony", "tom", "teddy"}, {"con", "mona", "don"}});
         int[] solution3 = sol.solution(new String[]{"may", "kein", "kain", "radi"}, new int[]{5, 10, 1, 3}, new String[][]{{"may"}, {"kein", "deny", "may"}, {"kon", "coni"}});
 
