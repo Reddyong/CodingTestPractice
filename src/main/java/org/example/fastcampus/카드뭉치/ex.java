fbf959cd95150a3c2e4a4a9e20289881e828abf9
Reddyong <d0209hong@naver.com>
2024-02-19 10:47:38 +0900

    feat : final answer of 카드 뭉치

--- src/main/java/org/example/fastcampus/카드뭉치/ex.java
+++ src/main/java/org/example/fastcampus/카드뭉치/ex.java
@@ -1,9 +1,38 @@
 package org.example.fastcampus.카드뭉치;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class ex {
+    Queue<String> q1 = new LinkedList<>();
+    Queue<String> q2 = new LinkedList<>();
     public String solution(String[] cards1, String[] cards2, String[] goal) {
         String answer = "";
+
+        init(cards1, cards2);
+        answer = makeGoal(goal);
+
         return answer;
+    }
+
+    private String makeGoal(String[] goal) {
+        for (String g : goal) {
+            if (g.equals(q1.peek())) {
+                q1.poll();
+            } else if (g.equals(q2.peek())) {
+                q2.poll();
+            } else {
+                return "No";
+            }
+        }
+
+        return "Yes";
+    }
+
+    private void init(String[] cards1, String[] cards2) {
+        q1.addAll(Arrays.asList(cards1));
+        q2.addAll(Arrays.asList(cards2));
     }
 
     public static void main(String[] args) {
