7119eba71194575c9531ba2e40e9eab955a4ecc8
Reddyong <d0209hong@naver.com>
2024-02-24 18:01:33 +0900

    feat : initList(), checkState() -> initList() : initialize extNum and sortNum by using checkState(), initialize saveList -> checkState() : return index of current num

--- src/main/java/org/example/fastcampus/Lv1/데이터분석/ex.java
+++ src/main/java/org/example/fastcampus/Lv1/데이터분석/ex.java
@@ -1,12 +1,48 @@
 package org.example.fastcampus.Lv1.데이터분석;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+
+import static com.sun.tools.javac.util.Context.checkState;
 
 public class ex {
+    int extNum;
+    int sortNum;
+    List<int[]> saveList = new ArrayList<>();
     public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
         int[][] answer;
 
+        initList(data, ext, val_ext, sort_by);
+
         return answer;
+    }
+
+    private void initList(int[][] data, String ext, int valExt, String sortBy) {
+        extNum = checkState(ext);
+        sortNum = checkState(sortBy);
+
+        for (int[] d : data) {
+            int temp = d[extNum];
+
+            if (valExt > temp) {
+                saveList.add(d);
+            }
+        }
+    }
+
+    private int checkState(String cur) {
+        if (cur.equals("code")) {
+            return 0;
+        }
+        if (cur.equals("date")) {
+            return 1;
+        }
+        if (cur.equals("maximum")) {
+            return 2;
+        }
+
+        return 3;
     }
 
     public static void main(String[] args) {
