179781c4fb8880c5cbb1e13f9ce8353e309c6ce1
Reddyong <d0209hong@naver.com>
2024-02-25 00:56:42 +0900

    feat : getResult() -> get answer using by list

--- src/main/java/org/example/fastcampus/Lv1/햄버거만들기/ex.java
+++ src/main/java/org/example/fastcampus/Lv1/햄버거만들기/ex.java
@@ -1,10 +1,39 @@
 package org.example.fastcampus.Lv1.햄버거만들기;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ex {
+    List<Integer> list = new ArrayList<>();
+    int temp = 0;
     public int solution(int[] ingredient) {
         int answer = 0;
 
+        getResult(ingredient);
+        answer = temp;
+
         return answer;
+    }
+
+    private void getResult(int[] ingredient) {
+        for (int i : ingredient) {
+            list.add(i);
+            while (list.size() > 3) {
+                int one = list.get(list.size() - 4);
+                int two = list.get(list.size() - 3);
+                int three = list.get(list.size() - 2);
+                int four = list.get(list.size() - 1);
+
+                if (one == 1 && two == 2 && three == 3 && four == 1) {
+                    for (int j = 0; j < 4; j++) {
+                        list.remove(list.size() - 1);
+                    }
+                    temp++;
+//                    break;
+                }
+                break;
+            }
+        }
     }
 
     public static void main(String[] args) {
