52d3d40d7fee839e77360d8eb1da7dd5b8482d46
Reddyong <d0209hong@naver.com>
2024-02-24 17:51:21 +0900

    feat : initial commit of 다음에 올 숫자

---
+++ src/main/java/org/example/fastcampus/Lv0/다음에올숫자/ex.java
@@ -0,0 +1,18 @@
+package org.example.fastcampus.Lv0.다음에올숫자;
+
+public class ex {
+    public int solution(int[] common) {
+        int answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ex sol = new ex();
+        int solution1 = sol.solution(new int[]{1, 2, 3, 4});
+        int solution2 = sol.solution(new int[]{2, 4, 8});
+
+        System.out.println("solution1 = " + solution1);
+        System.out.println("solution2 = " + solution2);
+    }
+}
