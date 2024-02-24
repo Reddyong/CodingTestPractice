654c10c4bc642d8396d84aa67cdae74756bb8968
Reddyong <d0209hong@naver.com>
2024-02-24 18:35:20 +0900

    feat : initial commit of 햄버거 만들기

---
+++ src/main/java/org/example/fastcampus/Lv1/햄버거만들기/ex.java
@@ -0,0 +1,18 @@
+package org.example.fastcampus.Lv1.햄버거만들기;
+
+public class ex {
+    public int solution(int[] ingredient) {
+        int answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ex sol = new ex();
+        int solution1 = sol.solution(new int[]{2, 1, 1, 2, 3, 1, 2, 3, 1});
+        int solution2 = sol.solution(new int[]{1, 3, 2, 1, 2, 1, 3, 1, 2});
+
+        System.out.println("solution1 = " + solution1);
+        System.out.println("solution2 = " + solution2);
+    }
+}
