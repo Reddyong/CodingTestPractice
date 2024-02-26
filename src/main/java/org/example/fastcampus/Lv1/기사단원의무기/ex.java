6b7f4e09fa96489e13803a42e618d6427c6c6c78
Reddyong <d0209hong@naver.com>
2024-02-26 20:57:10 +0900

    feat : initial commit of 기사단원의 무기

---
+++ src/main/java/org/example/fastcampus/Lv1/기사단원의무기/ex.java
@@ -0,0 +1,17 @@
+package org.example.fastcampus.Lv1.기사단원의무기;
+
+public class ex {
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ex sol = new ex();
+        int solution1 = sol.solution(5, 3, 2);
+        int solution2 = sol.solution(10, 3, 2);
+
+        System.out.println("solution1 = " + solution1);
+        System.out.println("solution2 = " + solution2);
+    }
+}
