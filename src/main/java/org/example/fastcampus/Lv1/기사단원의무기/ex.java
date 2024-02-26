99c4e6c6958b0535ab38171cea1a5c9a8ccf1284
Reddyong <d0209hong@naver.com>
2024-02-26 21:33:28 +0900

    feat : initArr(), getDivisors() -> initArr() : initialize numOfDiv[], each numbers of divisors are saved -> getDivisors() : get number of divisors of current number

--- src/main/java/org/example/fastcampus/Lv1/기사단원의무기/ex.java
+++ src/main/java/org/example/fastcampus/Lv1/기사단원의무기/ex.java
@@ -1,9 +1,42 @@
 package org.example.fastcampus.Lv1.기사단원의무기;
 
+import java.util.Arrays;
+
 public class ex {
+    int[] numOfDiv;
     public int solution(int number, int limit, int power) {
         int answer = 0;
+
+        initArr(number);
+
+
         return answer;
+    }
+
+    private void initArr(int number) {
+        numOfDiv = new int[number];
+
+        for (int i = 0; i < numOfDiv.length; i++) {
+            int cur = i + 1;
+            numOfDiv[i] = getDivisors(cur);
+        }
+    }
+
+    private int getDivisors(int cur) {
+        int num = (int) Math.sqrt(cur);
+        int cnt = 0;
+
+        for (int i = 1; i <= num; i++) {
+            if (cur % i == 0) {
+                cnt++;
+            }
+        }
+
+        if (num * num == cur) {
+            return cnt * 2 - 1;
+        }
+
+        return cnt * 2;
     }
 
     public static void main(String[] args) {
