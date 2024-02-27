68432eadbc49753667a799f422a155fb43fa39a4
Reddyong <d0209hong@naver.com>
2024-02-27 21:09:34 +0900

    feat : initial commit of 두개 뽑아서 더하기

---
+++ src/main/java/org/example/fastcampus/Lv1/두개뽑아서더하기/ex.java
@@ -0,0 +1,21 @@
+package org.example.fastcampus.Lv1.두개뽑아서더하기;
+
+import java.util.Arrays;
+
+public class ex {
+    public int[] solution(int[] numbers) {
+        int[] answer = {};
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ex sol = new ex();
+        int[] solution1 = sol.solution(new int[]{2, 1, 3, 4, 1});
+        int[] solution2 = sol.solution(new int[]{5, 0, 2, 7});
+
+        System.out.println("solution1 = " + Arrays.toString(solution1));
+        System.out.println("solution2 = " + Arrays.toString(solution2));
+    }
+}
