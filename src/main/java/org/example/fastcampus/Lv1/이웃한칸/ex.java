7b00555a3aa056015ebf256970e715fac528d7d6
Reddyong <d0209hong@naver.com>
2024-02-28 14:27:05 +0900

    feat : getSameColor(), isCorrectLocation() -> getSameColor() : check the right location of hCheck and wCheck, and compare is it equals with current h, w -> isCorrectLocation() : return true if is correct location, return false if is not correct location

--- src/main/java/org/example/fastcampus/Lv1/이웃한칸/ex.java
+++ src/main/java/org/example/fastcampus/Lv1/이웃한칸/ex.java
@@ -1,9 +1,39 @@
 package org.example.fastcampus.Lv1.이웃한칸;
 
 public class ex {
+    int[] dx = new int[]{0, 1, 0, -1};
+    int[] dy = new int[]{1, 0, -1, 0};
     public int solution(String[][] board, int h, int w) {
         int answer = 0;
+
+        answer = getSameColor(board, h, w);
         return answer;
+    }
+
+    private int getSameColor(String[][] board, int h, int w) {
+        int n = board.length;
+        int count = 0;
+
+        for (int i = 0; i < 4; i++) {
+            int hCheck = h + dy[i];
+            int wCheck = w + dx[i];
+
+            if (isCorrectLocation(hCheck, wCheck, n)) {
+                if (board[h][w].equals(board[hCheck][wCheck])) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    private boolean isCorrectLocation(int hCheck, int wCheck, int n) {
+        if (hCheck < 0 || wCheck < 0 || hCheck >= n || wCheck >= n) {
+            return false;
+        }
+
+        return true;
     }
 
     public static void main(String[] args) {
