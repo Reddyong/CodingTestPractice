2dd9e36438cc3b912af5128c1d9f1949226d636f
Reddyong <d0209hong@naver.com>
2024-02-28 14:22:21 +0900

    feat : getLowestScore() -> add score if it is bigger than lowest score, and get lowest score of current priority queue

--- src/main/java/org/example/fastcampus/Lv1/명예의전당1/ex.java
+++ src/main/java/org/example/fastcampus/Lv1/명예의전당1/ex.java
@@ -2,11 +2,40 @@ package org.example.fastcampus.Lv1.명예의전당1;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class ex {
+    PriorityQueue<Integer> pq = new PriorityQueue<>();
     public int[] solution(int k, int[] score) {
         int[] answer = {};
+
+        answer = getLowestScore(score, k);
         return answer;
+    }
+
+    private int[] getLowestScore(int[] score, int k) {
+        int[] ans = new int[score.length];
+        int count = 0;
+
+        while (count < score.length) {
+            if (pq.size() < k) {
+                pq.add(score[count]);
+                ans[count] = pq.peek();
+                count++;
+                continue;
+            }
+
+            int peek = pq.peek();
+            if (peek < score[count]) {
+                pq.poll();
+                pq.add(score[count]);
+            }
+
+            ans[count] = pq.peek();
+            count++;
+        }
+
+        return ans;
     }
 
     public static void main(String[] args) {
