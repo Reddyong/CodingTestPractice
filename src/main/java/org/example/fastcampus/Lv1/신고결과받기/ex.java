bec4d1776da91485fbd9cb3a7f79c02c41bc3545
Reddyong <d0209hong@naver.com>
2024-03-03 22:06:38 +0900

    feat : init() -> initialize maps(called, ansMap) and reportSet

--- src/main/java/org/example/fastcampus/Lv1/신고결과받기/ex.java
+++ src/main/java/org/example/fastcampus/Lv1/신고결과받기/ex.java
@@ -1,11 +1,28 @@
 package org.example.fastcampus.Lv1.신고결과받기;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class ex {
+    Map<String, Integer> called = new HashMap<>();
+    Map<String, Integer> ansMap = new HashMap<>();
+    Set<String> reportSet = new HashSet<>(); 
     public int[] solution(String[] id_list, String[] report, int k) {
-        int[] answer = {};
+        int[] answer = new int[id_list.length];
+
+        init(id_list, report);
+
         return answer;
+    }
+
+    private void init(String[] idList, String[] report) {
+        for (String id : idList) {
+            called.put(id, 0);
+            ansMap.put(id, 0);
+        }
+
+        for (String rep : report) {
+            reportSet.add(rep);
+        }
     }
 
     public static void main(String[] args) {
