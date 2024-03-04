b157fcd26cd5205dd34b3a33a273d1a836b561b1
Reddyong <d0209hong@naver.com>
2024-03-04 21:22:18 +0900

    feat : initial commit of 신규 아이디 추천

---
+++ src/main/java/org/example/fastcampus/Lv1/신규아이디추천/ex.java
@@ -0,0 +1,23 @@
+package org.example.fastcampus.Lv1.신규아이디추천;
+
+public class ex {
+    public String solution(String new_id) {
+        String answer = "";
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ex sol = new ex();
+        String solution1 = sol.solution("...!@BaT#*..y.abcdefghijklm");
+        String solution2 = sol.solution("z-+.^.");
+        String solution3 = sol.solution("=.=");
+        String solution4 = sol.solution("123_.def");
+        String solution5 = sol.solution("abcdefghijklmn.p");
+
+        System.out.println("solution1 = " + solution1);
+        System.out.println("solution2 = " + solution2);
+        System.out.println("solution3 = " + solution3);
+        System.out.println("solution4 = " + solution4);
+        System.out.println("solution5 = " + solution5);
+    }
+}
