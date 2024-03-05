cf1eb23a15da6c8b0049c3779be32ecd527fb718
Reddyong <d0209hong@naver.com>
2024-03-05 19:31:09 +0900

    feat : init(), changeLowerCase() -> init() : initialize saveSet, saveStr, saveId[] -> changeLowerCase() : change to lower case by using Character.isAlphabetic and Character.toLowerCase()

--- src/main/java/org/example/fastcampus/Lv1/신규아이디추천/ex.java
+++ src/main/java/org/example/fastcampus/Lv1/신규아이디추천/ex.java
@@ -1,9 +1,32 @@
 package org.example.fastcampus.Lv1.신규아이디추천;
 
+import java.util.*;
+
 public class ex {
+    Set<Character> signSet;
+    String saveStr;
+    char[] saveId;
     public String solution(String new_id) {
         String answer = "";
+
+        init(new_id);
+        changeLowerCase();
+
         return answer;
+    }
+
+    private void changeLowerCase() {
+        for (int i = 0; i < saveId.length; i++) {
+            if (Character.isAlphabetic(saveId[i])) {
+                saveId[i] = Character.toLowerCase(saveId[i]);
+            }
+        }
+    }
+
+    private void init(String newId) {
+        saveStr = "";
+        saveId = newId.toCharArray();
+        signSet = new HashSet<>(Set.of('-', '_', '.'));
     }
 
     public static void main(String[] args) {
