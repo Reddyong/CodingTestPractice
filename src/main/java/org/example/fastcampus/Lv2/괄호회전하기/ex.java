0bbc80b1e290c6feccbe108e0d6e1b7d12fb3c88
Reddyong <d0209hong@naver.com>
2024-03-05 20:09:37 +0900

    feat : getResult(), isCorrectBracket(), isMatched() -> getResult() : if it is correct bracket, ans++ and poll the queue and add again that thing -> isCorrectBracket() : use stack for checking it is matched, return true if stack is empty and return false if is not empty -> isMatched() : check the bracket is correct

--- src/main/java/org/example/fastcampus/Lv2/괄호회전하기/ex.java
+++ src/main/java/org/example/fastcampus/Lv2/괄호회전하기/ex.java
@@ -2,6 +2,7 @@ package org.example.fastcampus.Lv2.괄호회전하기;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 public class ex {
     Queue<Character> queue = new LinkedList<>();
@@ -9,8 +10,60 @@ public class ex {
         int answer = -1;
 
         init(s);
+        answer = getResult();
 
         return answer;
+    }
+
+    private int getResult() {
+        int ans = 0;
+        int count = 0;
+        int size = queue.size();
+
+        while (count < size) {
+            if (isCorrectBracket()) {
+                ans++;
+            }
+
+            Character poll = queue.poll();
+            queue.add(poll);
+            count++;
+        }
+
+        return ans;
+    }
+
+    private boolean isCorrectBracket() {
+        Stack<Character> stack = new Stack<>();
+
+        for (Character q : queue) {
+            if (!stack.isEmpty() && isMatched(stack.peek(), q)) {
+                stack.pop();
+                continue;
+            }
+
+            stack.push(q);
+        }
+
+        if (stack.isEmpty()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean isMatched(Character peek, Character q) {
+        if(peek == '(' && q == ')'){
+            return true;
+        }
+        if(peek == '{' && q == '}'){
+            return true;
+        }
+        if(peek == '[' && q == ']'){
+            return true;
+        }
+
+        return false;
     }
 
     private void init(String s) {
@@ -25,5 +78,10 @@ public class ex {
         int solution2 = sol.solution("}]()[{");
         int solution3 = sol.solution("[)(]");
         int solution4 = sol.solution("}}}");
+
+        System.out.println("solution1 = " + solution1);
+        System.out.println("solution2 = " + solution2);
+        System.out.println("solution3 = " + solution3);
+        System.out.println("solution4 = " + solution4);
     }
 }
