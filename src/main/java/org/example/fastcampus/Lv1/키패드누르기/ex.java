f3ec5b95796140c648d84a7f019ae3239e5c6901
Reddyong <d0209hong@naver.com>
2024-03-06 11:58:47 +0900

    feat : getResult(), checkHands(), getDistance() -> getResult() : change number to 11 if is 0, if is on left side, append L, if is on right side, append R, other cases have to checkHands() and append cur -> checkHands() : check the distance and return R or L -> getDistance() : get distance of the number and left or right, using Math.abs() / 3 and Maht.abs % e, because the key pad's one line has 3 numbers

--- src/main/java/org/example/fastcampus/Lv1/키패드누르기/ex.java
+++ src/main/java/org/example/fastcampus/Lv1/키패드누르기/ex.java
@@ -11,9 +11,73 @@ public class ex {
     public String solution(int[] numbers, String hand) {
         String answer = "";
 
-
+        answer = getResult(numbers, hand);
 
         return answer;
+    }
+
+    private String getResult(int[] numbers, String hand) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int number : numbers) {
+            if (number == 0) {
+                number = 11;
+            }
+
+            if (leftHand.contains(number)) {
+                left = number;
+                sb.append("L");
+                continue;
+            }
+            if (rightHand.contains(number)) {
+                right = number;
+                sb.append("R");
+                continue;
+            }
+
+            String cur = checkHands(number, hand);
+            sb.append(cur);
+        }
+
+        return sb.toString();
+    }
+
+    private String checkHands(int number, String hand) {
+        int leftDistance = 0;
+        int rightDistance = 0;
+
+        leftDistance = getDistance(number, "left");
+        rightDistance = getDistance(number, "right");
+
+        if (leftDistance > rightDistance) {
+            right = number;
+            return "R";
+        }
+        if (leftDistance < rightDistance) {
+            left = number;
+            return "L";
+        }
+
+        if (hand.equals("right")) {
+            right = number;
+            return "R";
+        }
+
+        left = number;
+        return "L";
+    }
+
+    private int getDistance(int number, String direction) {
+        int distance = 0;
+
+        if (direction.equals("left")) {
+            distance = Math.abs(number - left) / 3 + Math.abs(number - left) % 3;
+        }
+        if (direction.equals("right")) {
+            distance = Math.abs(number - right) / 3 + Math.abs(number - right) % 3;
+        }
+
+        return distance;
     }
 
     public static void main(String[] args) {
