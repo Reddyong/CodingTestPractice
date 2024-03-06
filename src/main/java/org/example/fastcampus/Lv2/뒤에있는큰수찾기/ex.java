a207158c791edd4abeb8c7d60dbcf4ddd45e3867
Reddyong <d0209hong@naver.com>
2024-03-06 12:01:28 +0900

    feat : initial commit of Lv.2 뒤에 있는 큰 수 찾기

---
+++ src/main/java/org/example/fastcampus/Lv2/뒤에있는큰수찾기/ex.java
@@ -0,0 +1,22 @@
+package org.example.fastcampus.Lv2.뒤에있는큰수찾기;
+
+import java.util.Arrays;
+
+public class ex {
+    public int[] solution(int[] numbers) {
+        int[] answer = {};
+
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ex sol = new ex();
+        int[] solution1 = sol.solution(new int[]{2, 3, 3, 5});
+        int[] solution2 = sol.solution(new int[]{9, 1, 5, 3, 6, 2});
+
+        System.out.println("solution1 = " + Arrays.toString(solution1));
+        System.out.println("solution2 = " + Arrays.toString(solution2));
+    }
+}
