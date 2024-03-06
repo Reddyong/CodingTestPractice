50a15ace347a2e91a97f46400d82f5bac81e42c9
Reddyong <d0209hong@naver.com>
2024-03-06 12:07:18 +0900

    feat : getResult() -> using stack which has the index of the numbers -> if stack is not empty and the peek number is smaller than current number, pop() and put in answer[] -> other case : push() -> finally the remaining numbers in stack, doesn't have the bigger number in array -> so put -1 in that idx

--- src/main/java/org/example/fastcampus/Lv2/뒤에있는큰수찾기/ex.java
+++ src/main/java/org/example/fastcampus/Lv2/뒤에있는큰수찾기/ex.java
@@ -16,7 +16,21 @@ public class ex {
     private int[] getResult(int[] numbers) {
         int[] ans = new int[numbers.length];
 
+        for (int i = 0; i < numbers.length; i++) {
+            while (!stack.isEmpty() && numbers[stack.peek()] < numbers[i]) {
+                Integer idx = stack.pop();
+                ans[idx] = numbers[i];
+            }
 
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            Integer idx = stack.pop();
+            ans[idx] = -1;
+        }
+
+        return ans;
     }
 
     public static void main(String[] args) {
