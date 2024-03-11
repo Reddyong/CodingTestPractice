55c1b29362140d27b342a791482688776ffdf72e
Reddyong <d0209hong@naver.com>
2024-03-11 10:31:09 +0900

    feat : findLowestLocation() -> find lux, luy, rdx, rdy : by using changeCoordinate() -> changeCoordinate() : compare with current i, j and change the value

--- src/main/java/org/example/fastcampus/Lv1/바탕화면정리/ex.java
+++ src/main/java/org/example/fastcampus/Lv1/바탕화면정리/ex.java
@@ -3,10 +3,43 @@ package org.example.fastcampus.Lv1.바탕화면정리;
 import java.util.Arrays;
 
 public class ex {
+    int lux = Integer.MAX_VALUE;
+    int luy = Integer.MAX_VALUE;
+    int rdx = Integer.MIN_VALUE;
+    int rdy = Integer.MIN_VALUE;
     public int[] solution(String[] wallpaper) {
         int[] answer = {};
 
+        findLowestLocation(wallpaper);
+
         return answer;
+    }
+
+    private void findLowestLocation(String[] wallpaper) {
+        for (int i = 0; i < wallpaper.length; i++) {
+            for (int j = 0; j < wallpaper[i].length(); j++) {
+                char cur = wallpaper[i].charAt(j);
+
+                if (cur == '#') {
+                    changeCoordinate(i, j);
+                }
+            }
+        }
+    }
+
+    private void changeCoordinate(int i, int j) {
+        if (i < luy) {
+            luy = i;
+        }
+        if (j < lux) {
+            lux = j;
+        }
+        if (i > rdy) {
+            rdy = i;
+        }
+        if (j > rdx) {
+            rdx = j;
+        }
     }
 
     public static void main(String[] args) {
