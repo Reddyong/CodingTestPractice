c3f8fb65e2e5016ca97f7a7f7497c070f4ae66fb
Reddyong <d0209hong@naver.com>
2024-03-11 10:41:38 +0900

    feat : initCurAndGoal() -> initialize visited[][] and curRx, curRy, goalX, goalY

--- src/main/java/org/example/fastcampus/Lv2/리코쳇로봇/ex.java
+++ src/main/java/org/example/fastcampus/Lv2/리코쳇로봇/ex.java
@@ -1,10 +1,39 @@
 package org.example.fastcampus.Lv2.리코쳇로봇;
 
 public class ex {
+    int curRx;
+    int curRy;
+    int goalX;
+    int goalY;
+    boolean[][] visited;
+    int[] dx = new int[]{1, 0, -1, 0};
+    int[] dy = new int[]{0, 1, 0, -1};
     public int solution(String[] board) {
         int answer = 0;
 
+        initCurAndGoal(board);
+
         return answer;
+    }
+
+    private void initCurAndGoal(String[] board) {
+        visited = new boolean[board.length][board[0].length()];
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length(); j++) {
+                char cur = board[i].charAt(j);
+
+                if (cur == 'R') {
+                    curRx = j;
+                    curRy = i;
+                }
+
+                if (cur == 'G') {
+                    goalX = j;
+                    goalY = i;
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
