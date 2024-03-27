48deebdc6f181c4417ee60e8531703a1bfdc7b96
Reddyong <d0209hong@naver.com>
2024-03-27 22:47:01 +0900

    feat : getHIndex() -> get H-Index with sorting citations

--- src/main/java/org/example/fastcampus/Lv2/H_Index/ex.java
+++ src/main/java/org/example/fastcampus/Lv2/H_Index/ex.java
@@ -1,11 +1,33 @@
 package org.example.fastcampus.Lv2.H_Index;
 
+import java.util.Arrays;
+
 public class ex {
     public int solution(int[] citations) {
         int answer = 0;
 
+        answer = getHIndex(citations);
 
         return answer;
+    }
+
+    private int getHIndex(int[] citations) {
+        int ans = 0;
+        int len = citations.length;
+
+        Arrays.sort(citations);
+
+        for (int i = 0; i < len; i++) {
+            int quotation = citations[i];
+            int moreThanQuo = len - i;
+
+            if (quotation >= moreThanQuo) {
+                ans = moreThanQuo;
+                break;
+            }
+        }
+
+        return ans;
     }
 
     public static void main(String[] args) {
