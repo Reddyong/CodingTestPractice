772151e2e557bf80d5d535630d0670ebe6be143b
Reddyong <d0209hong@naver.com>
2024-03-27 22:42:24 +0900

    feat : initial commit of H-Index

---
+++ src/main/java/org/example/fastcampus/Lv2/H_Index/ex.java
@@ -0,0 +1,17 @@
+package org.example.fastcampus.Lv2.H_Index;
+
+public class ex {
+    public int solution(int[] citations) {
+        int answer = 0;
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ex sol = new ex();
+        int solution = sol.solution(new int[]{3, 0, 6, 1, 5});
+
+        System.out.println("solution = " + solution);
+    }
+}
