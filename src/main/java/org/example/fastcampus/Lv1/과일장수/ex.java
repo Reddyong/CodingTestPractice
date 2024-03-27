57cba319257551d777225b2c8ecc7be287659ebb
Reddyong <d0209hong@naver.com>
2024-03-27 22:36:27 +0900

    feat : initial commit of 과일장수

---
+++ src/main/java/org/example/fastcampus/Lv1/과일장수/ex.java
@@ -0,0 +1,19 @@
+package org.example.fastcampus.Lv1.과일장수;
+
+public class ex {
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ex sol = new ex();
+        int solution1 = sol.solution(3, 4, new int[]{1, 2, 3, 1, 2, 3, 1});
+        int solution2 = sol.solution(4, 3, new int[]{4, 1, 2, 2, 4, 4, 4, 4, 1, 2, 4, 2});
+
+        System.out.println("solution1 = " + solution1);
+        System.out.println("solution2 = " + solution2);
+    }
+}
---
+++ src/main/java/org/example/fastcampus/Lv1/비밀지도/ex.java
@@ -0,0 +1,21 @@
+package org.example.fastcampus.Lv1.비밀지도;
+
+import java.util.*;
+
+public class ex {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = {};
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ex sol = new ex();
+        String[] solution1 = sol.solution(5, new int[]{9, 20, 28, 18, 11}, new int[]{30, 1, 21, 17, 28});
+        String[] solution2 = sol.solution(6, new int[]{46, 33, 33, 22, 31, 50}, new int[]{27, 56, 19, 14, 14, 10});
+
+        System.out.println("solution1 = " + Arrays.toString(solution1));
+        System.out.println("solution2 = " + Arrays.toString(solution2));
+    }
+}
