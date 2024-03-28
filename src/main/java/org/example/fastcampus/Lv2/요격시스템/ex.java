f2e3a06bf8dbdbcdb2d5f879255c99dcc1634d89
Reddyong <d0209hong@naver.com>
2024-03-28 17:29:42 +0900

    feat : initTargets() -> sort targets by asc targets[i][0]

--- src/main/java/org/example/fastcampus/Lv2/요격시스템/ex.java
+++ src/main/java/org/example/fastcampus/Lv2/요격시스템/ex.java
@@ -1,10 +1,18 @@
 package org.example.fastcampus.Lv2.요격시스템;
 
+import java.util.Arrays;
+
 public class ex {
     public int solution(int[][] targets) {
         int answer = 0;
 
+        initTargets(targets);
+
         return answer;
+    }
+
+    private void initTargets(int[][] targets) {
+        Arrays.sort(targets, ((o1, o2) -> o1[0] - o2[0]));
     }
 
     public static void main(String[] args) {
