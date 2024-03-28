1af3bb972e6e5fd55587b7bf2a82b3cf50b21cc3
Reddyong <d0209hong@naver.com>
2024-03-28 17:27:53 +0900

    feat : initial commit of 요격 시스템

---
+++ src/main/java/org/example/fastcampus/Lv2/요격시스템/ex.java
@@ -0,0 +1,16 @@
+package org.example.fastcampus.Lv2.요격시스템;
+
+public class ex {
+    public int solution(int[][] targets) {
+        int answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ex sol = new ex();
+        int solution = sol.solution(new int[][]{{4, 5}, {4, 8}, {10, 14}, {11, 13}, {5, 12}, {3, 7}, {1, 4}});
+
+        System.out.println("solution = " + solution);
+    }
+}
