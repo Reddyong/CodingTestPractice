ceb57a434bdce1916973af181758e7de26d41d76
Reddyong <d0209hong@naver.com>
2024-04-11 21:50:05 +0900

    feat : playDefense() -> get stage num of defense game

--- src/main/java/org/example/fastcampus/Lv2/디펜스게임/ex.java
+++ src/main/java/org/example/fastcampus/Lv2/디펜스게임/ex.java
@@ -1,12 +1,37 @@
 package org.example.fastcampus.Lv2.디펜스게임;
 
-import java.util.*;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class ex {
     public int solution(int n, int k, int[] enemy) {
         int answer = 0;
 
+        answer = playDefense(enemy, n, k);
+
         return answer;
+    }
+
+    private int playDefense(int[] enemy, int n, int k) {
+        int ans = 0;
+        Queue<Integer> pq = new PriorityQueue<>();
+
+        for (int e : enemy) {
+            n -= e;
+            pq.add(e);
+
+            if (n < 0) {
+                if (k <= 0) {
+                    break;
+                }
+                n += pq.poll();
+                k--;
+            }
+            ans++;
+        }
+
+        return ans;
     }
 
     public static void main(String[] args) {
