840c6f905e9826d9e32d6ccbfe9764b4c3084f89
Reddyong <d0209hong@naver.com>
2024-04-11 21:52:03 +0900

    feat : initial commit of 2 x n 타일링

---
+++ src/main/java/org/example/fastcampus/Lv2/nx2타일링/ex.java
@@ -0,0 +1,19 @@
+package org.example.fastcampus.Lv2.nx2타일링;
+
+public class ex {
+    private static final int DIVIDE_CASES = 1000000007;
+    int[] temp;
+    public int solution(int n) {
+        int answer = 0;
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ex sol = new ex();
+        int solution = sol.solution(4);
+
+        System.out.println("solution = " + solution);
+    }
+}
