877ac6c1931e1de198d1dca5a133354927bce2da
Reddyong <d0209hong@naver.com>
2024-04-11 21:45:58 +0900

    feat : initial commit of 디펜스 게임

---
+++ src/main/java/org/example/fastcampus/Lv2/디펜스게임/ex.java
@@ -0,0 +1,20 @@
+package org.example.fastcampus.Lv2.디펜스게임;
+
+import java.util.*;
+
+public class ex {
+    public int solution(int n, int k, int[] enemy) {
+        int answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ex sol = new ex();
+        int solution1 = sol.solution(7, 3, new int[]{4, 2, 4, 5, 3, 3, 1});
+        int solution2 = sol.solution(2, 4, new int[]{3, 3, 3, 3});
+
+        System.out.println("solution1 = " + solution1);
+        System.out.println("solution2 = " + solution2);
+    }
+}
