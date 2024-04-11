af5138ea6d66f18e0d0cfd693b350042afc1c815
Reddyong <d0209hong@naver.com>
2024-04-11 21:53:41 +0900

    feat : initTiles() -> initialize tiles[], first index and second index

--- src/main/java/org/example/fastcampus/Lv2/nx2타일링/ex.java
+++ src/main/java/org/example/fastcampus/Lv2/nx2타일링/ex.java
@@ -2,12 +2,25 @@ package org.example.fastcampus.Lv2.nx2타일링;
 
 public class ex {
     private static final int DIVIDE_CASES = 1000000007;
-    int[] temp;
+    int[] tiles;
     public int solution(int n) {
         int answer = 0;
 
+        initTiles(n);
 
         return answer;
+    }
+
+    private void initTiles(int n) {
+        tiles = new int[n];
+
+        if (n < 2) {
+            tiles[0] = 1;
+            return;
+        }
+
+        tiles[0] = 1;
+        tiles[1] = 2;
     }
 
     public static void main(String[] args) {
