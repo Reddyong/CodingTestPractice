2b7923ff0121cae1c18a217df82615934addb868
Reddyong <d0209hong@naver.com>
2024-04-12 12:23:11 +0900

    feat : getFailPerson() -> get failed stage and person num of game

--- src/main/java/org/example/fastcampus/Lv2/영어끝말잇기/ex.java
+++ src/main/java/org/example/fastcampus/Lv2/영어끝말잇기/ex.java
@@ -1,13 +1,44 @@
 package org.example.fastcampus.Lv2.영어끝말잇기;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ex {
+    int stage = 0;
+    int person = 0;
+    Set<String> wordSet = new HashSet<>();
     public int[] solution(int n, String[] words) {
         int[] answer = {};
 
+        answer = getFailPerson(n, words);
 
         return answer;
+    }
+
+    private int[] getFailPerson(int n, String[] words) {
+        int[] ans = new int[2];
+
+        for (int i = 0; i < words.length; i += n) {
+            stage++;
+            for (int j = i; j < i + n; j++) {
+                if (!wordSet.isEmpty()) {
+                    String prev = words[j - 1].substring(words[j - 1].length() - 1);
+                    String cur = words[j].substring(0, 1);
+
+                    if (wordSet.contains(words[j]) || !prev.equals(cur)) {
+                        person = j - i + 1;
+                        ans[0] = person;
+                        ans[1] = stage;
+
+                        return ans;
+                    }
+                }
+                wordSet.add(words[j]);
+            }
+        }
+
+        return ans;
     }
 
     public static void main(String[] args) {
