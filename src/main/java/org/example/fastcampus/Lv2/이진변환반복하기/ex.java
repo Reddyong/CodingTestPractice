7210974f36fd97f75467d8dc3c91ba35a6b691d1
Reddyong <d0209hong@naver.com>
2024-04-12 12:11:08 +0900

    feat : initial commit of 이진 변환 반복하기

---
+++ src/main/java/org/example/fastcampus/Lv2/이진변환반복하기/ex.java
@@ -0,0 +1,23 @@
+package org.example.fastcampus.Lv2.이진변환반복하기;
+
+import java.util.Arrays;
+
+public class ex {
+    public int[] solution(String s) {
+        int[] answer = {};
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ex sol = new ex();
+        int[] solution1 = sol.solution("110010101001");
+        int[] solution2 = sol.solution("01110");
+        int[] solution3 = sol.solution("1111111");
+
+        System.out.println("solution1 = " + Arrays.toString(solution1));
+        System.out.println("solution2 = " + Arrays.toString(solution2));
+        System.out.println("solution3 = " + Arrays.toString(solution3));
+    }
+}
