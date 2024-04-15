89ebce064b4ee3fd52a5e6550a14cbe02d76fe9d
Reddyong <d0209hong@naver.com>
2024-04-15 20:36:55 +0900

    feat : initial commit of 거리두기 확인하기

---
+++ src/main/java/org/example/fastcampus/Lv2/거리두기확인하기/ex.java
@@ -0,0 +1,19 @@
+package org.example.fastcampus.Lv2.거리두기확인하기;
+
+import java.util.Arrays;
+
+public class ex {
+    public int[] solution(String[][] places) {
+        int[] answer = {};
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ex sol = new ex();
+        int[] solution = sol.solution(new String[][]{{"POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"}, {"POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"}, {"PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"}, {"OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"}, {"PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"}});
+
+        System.out.println("solution = " + Arrays.toString(solution));
+    }
+}
