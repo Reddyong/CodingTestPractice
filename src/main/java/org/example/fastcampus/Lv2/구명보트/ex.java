8dd59beeb9b95fe0a69949f5f6430570647ab3fd
Reddyong <d0209hong@naver.com>
2024-04-15 20:29:43 +0900

    feat : initial commit of 구명 보트

---
+++ src/main/java/org/example/fastcampus/Lv2/구명보트/ex.java
@@ -0,0 +1,23 @@
+package org.example.fastcampus.Lv2.구명보트;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class ex {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ex sol = new ex();
+        int solution1 = sol.solution(new int[]{70, 50, 80, 50}, 100);
+        int solution2 = sol.solution(new int[]{70, 80, 50}, 100);
+
+        System.out.println("solution1 = " + solution1);
+        System.out.println("solution2 = " + solution2);
+    }
+}
