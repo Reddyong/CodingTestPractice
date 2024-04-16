e39128dfe58db95980c27f06522f7be564b2feaa
Reddyong <d0209hong@naver.com>
2024-04-16 18:28:25 +0900

    feat : initial commit of 멀리 뛰기

---
+++ src/main/java/org/example/fastcampus/Lv2/멀리뛰기/ex.java
@@ -0,0 +1,23 @@
+package org.example.fastcampus.Lv2.멀리뛰기;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class ex {
+    long[] dp;
+    public long solution(int n) {
+        long answer = 0;
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ex sol = new ex();
+        long solution1 = sol.solution(4);
+        long solution2 = sol.solution(3);
+
+        System.out.println("solution1 = " + solution1);
+        System.out.println("solution2 = " + solution2);
+    }
+}
