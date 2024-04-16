1ba84f5c8b936a36bda4ae7dba5f2b6604ff8c87
Reddyong <d0209hong@naver.com>
2024-04-16 18:35:20 +0900

    feat : sortData() -> sort data[][] by Arrays.sort and lambda expression -> sorting with conditions of problem

--- src/main/java/org/example/fastcampus/Lv2/테이블해시함수/ex.java
+++ src/main/java/org/example/fastcampus/Lv2/테이블해시함수/ex.java
@@ -1,11 +1,25 @@
 package org.example.fastcampus.Lv2.테이블해시함수;
 
+import java.util.Arrays;
+
 public class ex {
     public int solution(int[][] data, int col, int row_begin, int row_end) {
         int answer = 0;
 
+        sortData(data, col);
+
 
         return answer;
+    }
+
+    private void sortData(int[][] data, int col) {
+        Arrays.sort(data, ((o1, o2) -> {
+            if (o1[col - 1] == o2[col - 1]) {
+                return o2[0] - o1[0];
+            }
+
+            return o1[col - 1] - o2[col - 1];
+        }));
     }
 
     public static void main(String[] args) {
