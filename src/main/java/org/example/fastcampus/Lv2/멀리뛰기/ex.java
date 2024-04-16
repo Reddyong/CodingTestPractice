9e14b984c3c5b3f64167a367276e5158f6aace15
Reddyong <d0209hong@naver.com>
2024-04-16 18:30:24 +0900

    feat : getResult() -> get result by using dp algorithm

--- src/main/java/org/example/fastcampus/Lv2/멀리뛰기/ex.java
+++ src/main/java/org/example/fastcampus/Lv2/멀리뛰기/ex.java
@@ -8,8 +8,30 @@ public class ex {
     public long solution(int n) {
         long answer = 0;
 
+        answer = getResult(n);
 
         return answer;
+    }
+
+    private long getResult(int n) {
+        dp = new long[n];
+
+        if (n == 1) {
+            return 1;
+        }
+
+        if (n == 2) {
+            return 2;
+        }
+
+        dp[0] = 1;
+        dp[1] = 2;
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = dp[i - 1] % 1234567 + dp[i - 2] % 1234567;
+        }
+
+        return dp[n - 1] % 1234567;
     }
 
     public static void main(String[] args) {
