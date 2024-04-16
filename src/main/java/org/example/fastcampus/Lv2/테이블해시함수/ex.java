c7ae71faad2400fafde0d7fc582062e4e104a121
Reddyong <d0209hong@naver.com>
2024-04-16 18:32:45 +0900

    feat : initial commit of 테이블 해시 함수

---
+++ src/main/java/org/example/fastcampus/Lv2/테이블해시함수/ex.java
@@ -0,0 +1,17 @@
+package org.example.fastcampus.Lv2.테이블해시함수;
+
+public class ex {
+    public int solution(int[][] data, int col, int row_begin, int row_end) {
+        int answer = 0;
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ex sol = new ex();
+        int solution = sol.solution(new int[][]{{2, 2, 6}, {1, 5, 10}, {4, 2, 9}, {3, 8, 3}}, 2, 2, 3);
+
+        System.out.println("solution = " + solution);
+    }
+}
