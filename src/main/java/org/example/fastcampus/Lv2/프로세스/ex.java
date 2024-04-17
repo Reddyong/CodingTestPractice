968569179cd284d618c984a8c46a1f09bdcff255
Reddyong <d0209hong@naver.com>
2024-04-17 15:52:34 +0900

    feat : initial commit of 프로세스

---
+++ src/main/java/org/example/fastcampus/Lv2/프로세스/ex.java
@@ -0,0 +1,19 @@
+package org.example.fastcampus.Lv2.프로세스;
+
+public class ex {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ex sol = new ex();
+        int solution1 = sol.solution(new int[]{2, 1, 3, 2}, 2);
+        int solution2 = sol.solution(new int[]{1, 1, 9, 1, 1, 1}, 0);
+
+        System.out.println("solution1 = " + solution1);
+        System.out.println("solution2 = " + solution2);
+    }
+}
