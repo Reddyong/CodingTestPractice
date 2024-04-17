de25f51bcbfd29a8e1c696af442bf8ae4cd12d21
Reddyong <d0209hong@naver.com>
2024-04-17 15:47:10 +0900

    feat : initial commit of N-Queen

---
+++ src/main/java/org/example/fastcampus/Lv2/NQueen/ex.java
@@ -0,0 +1,18 @@
+package org.example.fastcampus.Lv2.NQueen;
+
+public class ex {
+    int[] queen;
+    int answer;
+    public int solution(int n) {
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ex sol = new ex();
+        int solution = sol.solution(4);
+
+        System.out.println("solution = " + solution);
+    }
+}
