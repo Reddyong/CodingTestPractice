47160afb0e442d4397d8bfa79848e772fa605742
Reddyong <d0209hong@naver.com>
2024-04-19 12:07:02 +0900

    feat : getResult(), getMaxY() -> getResult() : get result of problem by using getMaxY() -> getMaxY() : get max y, by using Math.sqrt() and Math.pow()

--- src/main/java/org/example/fastcampus/Lv2/점찍기/ex.java
+++ src/main/java/org/example/fastcampus/Lv2/점찍기/ex.java
@@ -4,8 +4,25 @@ public class ex {
     public long solution(int k, int d) {
         long answer = 0;
 
+        answer = getResult(k, d);
 
         return answer;
+    }
+
+    private long getResult(int k, int d) {
+        long ans = 0;
+
+        for (int i = 0; i <= d; i += k) {
+            long maxY = getMaxY(i, d);
+            ans += maxY / k;
+            ans++;
+        }
+
+        return ans;
+    }
+
+    private long getMaxY(int i, int d) {
+        return (long) Math.sqrt(Math.pow(d, 2) - Math.pow(i, 2));
     }
 
     public static void main(String[] args) {
