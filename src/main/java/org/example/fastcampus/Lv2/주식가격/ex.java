6ffc7b7f7bb98f6e1c78c2907b85a247ec6f42ef
Reddyong <d0209hong@naver.com>
2024-04-19 11:36:53 +0900

    feat : initial commit of 주식 가격

---
+++ src/main/java/org/example/fastcampus/Lv2/주식가격/ex.java
@@ -0,0 +1,19 @@
+package org.example.fastcampus.Lv2.주식가격;
+
+import java.util.Arrays;
+
+public class ex {
+    public int[] solution(int[] prices) {
+        int[] answer = {};
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ex sol = new ex();
+        int[] solution = sol.solution(new int[]{1, 2, 3, 2, 3});
+
+        System.out.println("solution = " + Arrays.toString(solution));
+    }
+}
