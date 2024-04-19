9117b43f9d08fd2629accd913da5e683df105db4
Reddyong <d0209hong@naver.com>
2024-04-19 11:40:34 +0900

    feat : getResult(), getTerm() -> get result of problem by comparing with next numbers -> getTerm() : get the term of time until meet bigger number

--- src/main/java/org/example/fastcampus/Lv2/주식가격/ex.java
+++ src/main/java/org/example/fastcampus/Lv2/주식가격/ex.java
@@ -6,8 +6,38 @@ public class ex {
     public int[] solution(int[] prices) {
         int[] answer = {};
 
+        answer = getResult(prices);
 
         return answer;
+    }
+
+    private int[] getResult(int[] prices) {
+        int[] ans = new int[prices.length];
+
+        for (int i = 0; i < prices.length; i++) {
+            int cur = prices[i];
+
+            int term = getTerm(i + 1, cur, prices);
+
+            ans[i] = term;
+        }
+
+        return ans;
+    }
+
+    private int getTerm(int idx, int cur, int[] prices) {
+        int count = 0;
+
+        for (int i = idx; i < prices.length; i++) {
+            if (cur > prices[i]) {
+                count++;
+                break;
+            }
+
+            count++;
+        }
+
+        return count;
     }
 
     public static void main(String[] args) {
