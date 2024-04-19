49a882cd7719a883d97ecfc8bb0add412fb7dbbb
Reddyong <d0209hong@naver.com>
2024-04-19 11:48:44 +0900

    feat : initial commit of 점 찍ㄱ

---
+++ src/main/java/org/example/fastcampus/Lv2/점찍기/ex.java
@@ -0,0 +1,19 @@
+package org.example.fastcampus.Lv2.점찍기;
+
+public class ex {
+    public long solution(int k, int d) {
+        long answer = 0;
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ex sol = new ex();
+        long solution1 = sol.solution(2, 4);
+        long solution2 = sol.solution(1, 5);
+
+        System.out.println("solution1 = " + solution1);
+        System.out.println("solution2 = " + solution2);
+    }
+}
