55e5cdca7967c8d95c59ca7b34d15067860c830d
Reddyong <d0209hong@naver.com>
2024-04-19 11:20:36 +0900

    feat : initial commit of 삼각 달팽이

---
+++ src/main/java/org/example/fastcampus/Lv2/귤고르기/ex.java
@@ -0,0 +1,23 @@
+package org.example.fastcampus.Lv2.귤고르기;
+
+import java.util.*;
+
+public class ex {
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(1, 3);
+        map.put(2, 3);
+
+        List<Integer> list = new ArrayList<>(map.values());
+        System.out.println("list = " + list);
+
+    }
+}
---
+++ src/main/java/org/example/fastcampus/Lv2/삼각달팽이/ex.java
@@ -0,0 +1,22 @@
+package org.example.fastcampus.Lv2.삼각달팽이;
+
+import java.util.Arrays;
+
+public class ex {
+    public int[] solution(int n) {
+        int[] answer = {};
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ex sol = new ex();
+        int[] solution1 = sol.solution(4);
+        int[] solution2 = sol.solution(5);
+        int[] solution3 = sol.solution(6);
+
+        System.out.println("solution1 = " + Arrays.toString(solution1));
+        System.out.println("solution2 = " + Arrays.toString(solution2));
+        System.out.println("solution3 = " + Arrays.toString(solution3));
+    }
+}
