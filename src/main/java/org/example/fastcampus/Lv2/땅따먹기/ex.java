5c2d566bc8bd560af4d5700a99880d77e48e5ed2
Reddyong <d0209hong@naver.com>
2024-04-23 22:38:51 +0900

    feat : getMaxLand() -> get max land with using dp algorithm

--- src/main/java/org/example/fastcampus/Lv2/땅따먹기/ex.java
+++ src/main/java/org/example/fastcampus/Lv2/땅따먹기/ex.java
@@ -4,8 +4,27 @@ public class ex {
     int solution(int[][] land) {
         int answer = 0;
 
+        answer = getMaxLand(land);
 
         return answer;
+    }
+
+    private int getMaxLand(int[][] land) {
+        int ans = 0;
+        int N = land.length - 1;
+
+        for (int i = 1; i < land.length; i++) {
+            land[i][0] += Math.max(land[i - 1][1], Math.max(land[i - 1][2], land[i - 1][3]));
+            land[i][1] += Math.max(land[i - 1][0], Math.max(land[i - 1][2], land[i - 1][3]));
+            land[i][2] += Math.max(land[i - 1][0], Math.max(land[i - 1][1], land[i - 1][3]));
+            land[i][3] += Math.max(land[i - 1][0], Math.max(land[i - 1][1], land[i - 1][2]));
+        }
+
+        for (int j = 0; j < 4; j++) {
+            ans = Math.max(ans, land[N][j]);
+        }
+
+        return ans;
     }
 
     public static void main(String[] args) {
