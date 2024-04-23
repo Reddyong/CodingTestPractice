844d923a4486c805beab5cd87b8f46308dfa3356
Reddyong <d0209hong@naver.com>
2024-04-23 22:24:11 +0900

    feat : init() -> initialize dp[][] and R, N

--- src/main/java/org/example/fastcampus/Lv2/가장큰정사각형찾기/ex.java
+++ src/main/java/org/example/fastcampus/Lv2/가장큰정사각형찾기/ex.java
@@ -1,10 +1,29 @@
 package org.example.fastcampus.Lv2.가장큰정사각형찾기;
 
 public class ex {
+    int[][] dp;
+    int R;
+    int N;
     public int solution(int[][] board){
         int answer = 1234;
 
+        init(board);
+
         return answer;
+    }
+
+    private void init(int[][] board) {
+        R = board.length;
+        N = board[0].length;
+        dp = new int[R][N];
+
+        for (int i = 0; i < R; i++) {
+            dp[i][0] = board[i][0];
+        }
+
+        for (int j = 0; j < N; j++) {
+            dp[0][j] = board[0][j];
+        }
     }
 
     public static void main(String[] args) {
