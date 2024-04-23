2bd83053245272e911969709fc0a66ffee136617
Reddyong <d0209hong@naver.com>
2024-04-23 22:35:14 +0900

    feat : initial commit of 땅따먹기

---
+++ src/main/java/org/example/fastcampus/Lv2/땅따먹기/ex.java
@@ -0,0 +1,17 @@
+package org.example.fastcampus.Lv2.땅따먹기;
+
+public class ex {
+    int solution(int[][] land) {
+        int answer = 0;
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ex sol = new ex();
+        int solution = sol.solution(new int[][]{{1, 2, 3, 5}, {5, 6, 7, 8}, {4, 3, 2, 1}});
+
+        System.out.println("solution = " + solution);
+    }
+}
