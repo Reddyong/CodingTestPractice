c7f9f4ed09f5fbb5abb870be5d760a7631547fae
Reddyong <d0209hong@naver.com>
2024-04-24 20:44:30 +0900

    feat : getTotalPair(), getCurLength() -> get total pair num by using getCurLength, Math.pow(), Math.sqrt

--- src/main/java/org/example/fastcampus/Lv2/두원사이의정수쌍/ex.java
+++ src/main/java/org/example/fastcampus/Lv2/두원사이의정수쌍/ex.java
@@ -4,9 +4,42 @@ public class ex {
     public long solution(int r1, int r2) {
         long answer = 0;
 
-
+        answer = getTotalPair(r1, r2);
 
         return answer;
+    }
+
+    private long getTotalPair(int r1, int r2) {
+        long temp = 0;
+        long l1 = 0;
+        long l2 = 0;
+
+        for (int i = 0; i <= r2; i++) {
+            if (i == 0) {
+                temp += (r2 - r1 + 1);
+                continue;
+            }
+
+            l1 = getCurLength(r1, i);
+            l2 = getCurLength(r2, i);
+
+            if (i >= r1) {
+                temp += l2;
+                continue;
+            }
+
+            if (Math.sqrt(Math.pow(r1, 2) - Math.pow(i, 2)) % 1 == 0) {
+                temp++;
+            }
+
+            temp += (l2 - l1);
+        }
+
+        return temp * 4;
+    }
+
+    private long getCurLength(int r, int x) {
+        return (long) Math.sqrt((long) Math.pow(r, 2) - (long) Math.pow(x, 2));
     }
 
     public static void main(String[] args) {
