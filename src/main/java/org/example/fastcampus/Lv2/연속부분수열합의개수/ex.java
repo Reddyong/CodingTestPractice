2b1d0892e38e4d0ead819fa6d9d59050e7d9e334
Reddyong <d0209hong@naver.com>
2024-04-24 20:34:51 +0900

    feat : initial commit of 연속 부분 수열 합의 개수

---
+++ src/main/java/org/example/fastcampus/Lv2/연속부분수열합의개수/ex.java
@@ -0,0 +1,18 @@
+package org.example.fastcampus.Lv2.연속부분수열합의개수;
+
+public class ex {
+    public int solution(int[] elements) {
+        int answer = 0;
+
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ex sol = new ex();
+        int solution = sol.solution(new int[]{7, 9, 1, 1, 4});
+
+        System.out.println("solution = " + solution);
+    }
+}
