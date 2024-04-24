f5e566aeca150e479099ef8bb642ed1323d1b147
Reddyong <d0209hong@naver.com>
2024-04-24 20:39:42 +0900

    feat : initial commit of 두 원 사이의 정수 쌍

---
+++ src/main/java/org/example/fastcampus/Lv2/두원사이의정수쌍/ex.java
@@ -0,0 +1,18 @@
+package org.example.fastcampus.Lv2.두원사이의정수쌍;
+
+public class ex {
+    public long solution(int r1, int r2) {
+        long answer = 0;
+
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ex sol = new ex();
+        long solution = sol.solution(2, 3);
+
+        System.out.println("solution = " + solution);
+    }
+}
