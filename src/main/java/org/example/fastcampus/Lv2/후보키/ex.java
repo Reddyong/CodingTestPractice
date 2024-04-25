00243ce43cb4ea2dde4a89a099272cb9f991ce8c
Reddyong <d0209hong@naver.com>
2024-04-25 14:14:24 +0900

    feat : initial commit of 후보키

---
+++ src/main/java/org/example/fastcampus/Lv2/후보키/ex.java
@@ -0,0 +1,16 @@
+package org.example.fastcampus.Lv2.후보키;
+
+public class ex {
+    public int solution(String[][] relation) {
+        int answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ex sol = new ex();
+        int solution = sol.solution(new String[][]{{"100", "ryan", "music", "2"}, {"200", "apeach", "math", "2"}, {"300", "tube", "computer", "3"}, {"400", "con", "computer", "4"}, {"500", "muzi", "music", "3"}, {"600", "apeach", "music", "2"}});
+
+        System.out.println("solution = " + solution);
+    }
+}
